14aaeb4b1096733abe81f2af79dbca1143503390
Devin Givner <DGivner@TheRockBrook.com>
2023-01-29 08:56:38 -0500

    Add project files.

---
+++ RAA_WPF_Session01_Challenge_Project_Setup/Utils/CommandAvailability.cs
@@ -0,0 +1,25 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAA_WPF_Session01_Challenge_Project_Setup
+{
+    internal class CommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+        {
+            bool result = false;
+            UIDocument activeDoc = applicationData.ActiveUIDocument;
+            if (activeDoc != null && activeDoc.Document != null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
